81d405aaf24ec37a68e06a3dff93529fdc68ce53
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2026-02-10 18:47:01 +0300

    deploy

--- index.4d260.js
+++ index.4d260.js
@@ -1,4 +1,4 @@
-System.register(["./application.7d1e8.js"], function (_export, _context) {
+System.register(["./application.74d6d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
