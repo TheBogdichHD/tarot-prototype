74b796cd325ba6712213ae1fa39d3facdd048d5f
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2026-02-20 20:50:52 +0300

    sssdfg

--- application.c51ec.js
+++ application.c51ec.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.6f291.json';
+          this.settingsPath = 'src/settings.19b1b.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.eed08.js
+++ index.eed08.js
@@ -1,4 +1,4 @@
-System.register(["./application.74d6d.js"], function (_export, _context) {
+System.register(["./application.c51ec.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
